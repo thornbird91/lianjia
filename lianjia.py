b02154e02be637a3782930ad430bee6ad1d472ad
conleyhao <274498361@qq.com>
2017-08-17 20:04:06 +0800

    time label is not OK

--- lianjia.py
+++ lianjia.py
@@ -1,14 +1,14 @@
 import datetime
 import requests
 from bs4 import BeautifulSoup
-import numpy as np
+import pylab as pl
 import matplotlib.pyplot as plt
 
-lj_hs = []
-wj_hs = []
-today = []
+lj_hs = open('lj_hs.txt', 'a')
+wj_hs = open('wj_hs.txt', 'a')
+date = open('date.txt', 'a')
 
-def get_house_num():
+def get_hs():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Accept': 'text/html;q=0.9,*/*;q=0.8',
@@ -23,39 +23,91 @@ def get_house_num():
     lj_html = lj_r.content
     lj = BeautifulSoup(lj_html, 'html.parser')
     lj_num_ct = lj.find('div', attrs={'class': 'resultDes clear'})
-    lj_num = lj_num_ct.span.string
-    lj_hs.append(lj_num)
+    lj_num = lj_num_ct.span.string.strip()
+    lj_hs.write(lj_num + ' ')
+    lj_hs.close()
 
-    print(lj_num)
+    #print(lj_num)
 
     wawj_url = 'http://hz.5i5j.com/exchange'
     wawj_r = requests.get(url=wawj_url, headers=headers)
     wawj_html = wawj_r.content
     wawj = BeautifulSoup(wawj_html, 'html.parser')
     wawj_num_cnt = wawj.find('font', attrs={'class': 'font-houseNum'})
-    wawj_num = wawj_num_cnt.string
-    wj_hs.append(wawj_num)
+    wawj_num = wawj_num_cnt.string.strip()
+    wj_hs.write(wawj_num + ' ')
+    wj_hs.close()
 
-    print(wawj_num)
+    #print(wawj_num)
 
 
 def get_time():
     now_time = datetime.datetime.now()
     today_time = now_time.strftime('%m/%d')
-    today.append(today_time)
+    date.write(today_time + ' ')
+    date.close()
 
-def fig_plot(today, lj_hs, wj_hs):
+def fig_plot():
+    global date_list
+    lj_hs_rd = open('lj_hs.txt', 'r')
+    wj_hs_rd = open('wj_hs.txt', 'r')
+    date_rd  = open('date.txt', 'r')
+
+    lj_ay = []
+    wj_ay = []
+    dt_ay = []
+
+    for lj_line in lj_hs_rd.readlines():
+        lj_list = lj_line.split(' ')
+        for i in range(len(lj_list)-1):
+            aa = int(lj_list[i])
+            lj_ay.append(aa)
+
+    for wj_line in wj_hs_rd.readlines():
+        wj_list = wj_line.split(' ')
+        for j in range(len(wj_list)-1):
+            bb = int(wj_list[j])
+            wj_ay.append(bb)
+
+    for date_line in date_rd.readlines():
+        date_list = date_line.split(' ')
+        dt_list = date_list.pop()
+        for k in range(len(date_list)-1):
+            print(date_list[k])
+            dt = datetime.datetime.strptime(date_list[k], '%m/%d')
+            print(dt, type(dt))
 
 
-#if __name__ == '__main__':
-#    get_hs()
-#    get_time()
+    #dt = datetime.datetime.strptime(dt_list)
 
-x = [1,2,3,4,5]
-y = [1,4,9,16,25]
-z = [1,1,2,4,8]
+    #print(lj_ay)
+    #print(wj_ay)
+    #print(dt_list, type(dt_list))
 
-plt.plot(x,y)
-plt.plot(x,z)
-plt.show()
+    pl.plot_date(pl.date2num(dt_ay),lj_ay, label = 'lj hosue number')
+    pl.plot_date(pl.date2num(dt_ay),wj_ay, label = 'wawj house number')
+    plt.xlabel('date')
+    plt.ylabel('house number')
+    plt.grid(True)
+    plt.legend(loc = 'upper left')
+    plt.show()
+
+
+if __name__ == '__main__':
+    get_hs()
+    get_time()
+    fig_plot()
+
+#x = [1,2,3,4,5]
+#y = [1,4,9,16,25]
+#z = [1,1,2,4,8]
+#
+#plot1 = plt.plot(x,y, label = 'lj hosue number')
+#plot2 = plt.plot(x,z, label = 'wawj house number')
+#plt.xlabel('date')
+#plt.ylabel('house number')
+#plt.grid(True)
+#plt.legend(loc = 'upper left')
+
+#plt.show()
 
